4202c795cd23ec84ebd18f7ef4e7fb8e1561f085
leonbicak <131712809+leonbicak@users.noreply.github.com>
2023-10-27 00:43:53 +0200

    dodavanje stranice

--- front/02_RWD/js/app.js
+++ front/02_RWD/js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation()
+$(document).foundation();
+
+$.ajax('https://localhost:7116/api/v1/kupac2',   // request url
+    {
+        success: function (data, status, xhr) {// success callback function
+            //console.log(data);
+            for(let i=0;i<data.length;i++){
+                $('#podaci').append('<li>' + data[i].ime + '</li>');
+
+            }
+    }
+});
